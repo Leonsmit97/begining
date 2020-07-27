061b6b0ad96cbdfa8ccafb46d7264cc4bd4c4dff
Leonsmit97 <68805738+Leonsmit97@users.noreply.github.com>
2020-07-27 19:11:06 +1000

    Sound V 1.0.0

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace sound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                SoundPlayer player = new SoundPlayer(@"C:\Users\Leon\Documents\sound.wav");
+                player.Play();
+                Console.ReadLine();
+            }
+          
+        }
+    }
+}
